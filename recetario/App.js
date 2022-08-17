565eacefd2a88c3cbdb1765835d8d41fe54e0769
Lucas Manuel <49735490+Lucas-aquiles@users.noreply.github.com>
2022-08-17 19:16:53 -0300

    add changes
    Co-authored-by: Antonio3veces3 <cramirez28@ucol.mx>
    Co-authored-by: Astroyayo <lugo044@gmail.com>

--- recetario/App.js
+++ recetario/App.js
@@ -1,36 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import SearchBarComponent from "./components/search-bar.js";
 import { styles } from "./styles/styleApp"
-import TrendingList from './components/trending-list.js';
-import * as data from './resources/data.json';
-
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.component} >
-        <SearchBarComponent />
-      </View>
-
-
-      <View style={styles.viewTrendingList}>
-        <TrendingList
-          title={"TRENDING"}
-          width={90}
-          height={70}
-          data={data.dishes}
-        />
-        <TrendingList
-          title={"RECENT"}
-          width={110}
-          height={120}
-          data={data.dishes}
-        />
-      </View>
-
-    </View>
+    
   );
 }
 
