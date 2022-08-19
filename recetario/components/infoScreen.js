49889af11d9e5bbebc94b9eff9bc0a7ef78f2373
Antonio3veces3 <cramirez28@ucol.mx>
2022-08-19 17:40:55 -0500

    add(icons) Add icons on the infoScreen
    Co-authored-by: Astroyayo <lugo044@gmail.com>
    Co-authored-by: Lucas-aquiles <lucasechegaray2011@gmail.com>

--- recetario/components/infoScreen.js
+++ recetario/components/infoScreen.js
@@ -1,25 +1,35 @@
 import * as React from 'react';
 import { View, Text, TouchableOpacity, Image, ImageBackground, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Icon } from "@rneui/themed"; 
 
 const InfoScreen = ({ route, navigation }) => {
     const item = route.params;
 
     return (
-        <View style={styles.container}>
-            <ImageBackground
-                source={{ uri: item.img }}
-                resizeMode="cover"
-                style={styles.imgBackground}>
+            <View style={styles.container}>
+                <View style={styles.viewImage}>
+                    <ImageBackground
+                    source={{ uri: item.img }}
+                    resizeMode="cover"
+                    style={styles.imgBackground}>
+                    <View style={styles.iconsView}>
+                    <Icon name= "close" color={"#FFFF"} type="material-community" size={40} onPress={() =>
+                        navigation.navigate('MainScreen')}/>
+                    <Icon name= "export-variant" color={"#FFFF"} type="material-community" size={40}/>
+                    <Icon name= "heart-outline" color={"#FFFF"} type="material-community" size={40}/>
+                    </View>
 
-                <Text>{item.recent ? 'Recent' : 'Trending'}</Text>
-                <Text>{item.title}</Text>
-                <TouchableOpacity
-                    onPress={() =>
-                        navigation.navigate('MainScreen')}
-                >
-                    <Text>Go to Home</Text>
-                </TouchableOpacity>
-            </ImageBackground>
+                    <Text style={styles.section}>{item.recent ? 'Recent' : 'Trending'}</Text>
+                    <Text style={styles.title}>{item.title}</Text>
+                    </ImageBackground>
+                </View>
+            
+
+            <View style={styles.viewServing}>
+                <Text>Ingredients</Text>
+                <Text>for {item.recipe.servings} servings</Text>
+            </View>
         </View>
     );
 }
@@ -28,9 +38,32 @@ const styles = StyleSheet.create({
     container: {
         flex: 1, alignItems: 'center'
     },
+    section:{
+        fontSize: 20,
+        color: 'white',
+    },
+    title:{
+        fontSize: 25,
+        color: 'white',
+    },
+    viewImage:{
+        width: "100%",
+        height: "45%",
+        backgroundColor: 'gray'
+    },
     imgBackground: {
         width: "100%",
-        height: "50%"
+        height: "100%",
+        
+    },
+    viewServing: {
+        height: 70,
+        width: '100%',
+    },
+    iconsView:{
+        flexDirection: 'row',
+        paddingTop: 30,
+        opacity: 1,
     }
 });
 export default InfoScreen;
